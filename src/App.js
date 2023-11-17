3d8f3699000da499d25a79478a3eace2d661e309
Timo Asumaniemi <asumaniemi.timo@outlook.com>
2023-11-17 18:15:50 +0200

    Moved routes from App.js to RoutesComponent.js
    Move routes to own RoutesComponent module from App.js
    Rename index.js to RoutesComponent.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./common/routes/Home";
 import Navbar from "./common/components/Navbar/Navbar";
-import DisplayProducts from "./common/routes/DisplayProducts";
-import ProductDetail from "./common/components/ProductDetail/ProductDetail";
 import Footer from "./common/components/Footer/Footer";
+import RoutesComponent from "./common/routes/RoutesComponent";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/displayProducts" element={<DisplayProducts />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-        </Routes>
+        <RoutesComponent />
         <Footer />
       </div>
     </Router>
