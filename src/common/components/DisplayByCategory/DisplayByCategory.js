f4017415a93a1905e5c06561c61721e201824032
Kagari94 <jesse.hirvo@hotmail.com>
2023-11-22 19:23:49 +0200

    Added DisplayByCategory.js and removed it from .gitignore. modified:   .gitignore new file:   src/common/components/DisplayByCategory/DisplayByCategory.js

---
+++ src/common/components/DisplayByCategory/DisplayByCategory.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ProductCard from "../ProductCard/ProductCard";
+
+// server link for products
+const productsURL = "https://big.kapsi.fi/products";
+
+const DisplayByCategory = ({ selectedCategory }) => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(productsURL)
+      .then((res) => {
+        // Filter products based on selected category
+        const filteredProducts = selectedCategory
+          ? res.data.filter((product) => product.category === selectedCategory.categoryName)
+          : res.data;
+        setProducts(filteredProducts)
+      })
+      .catch((err) => console.log(err.message));
+      
+  }, [selectedCategory]);
+
+  return (
+    <div className="container p-4">
+      <div className="row">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DisplayByCategory;
