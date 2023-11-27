11172062dc8efd899736dd491bd0f259aa22890f
laovirtanen <=>
2023-11-28 01:25:53 +0200

    footer and navbar adjustments

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./common/components/Navbar/Navbar";
 import Footer from "./common/components/Footer/Footer";
 import RoutesComponent from "./common/routes/RoutesComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <RoutesComponent />
+        <div className="content-wrapper">
+          <RoutesComponent />
+        </div>
         <Footer />
       </div>
     </Router>
