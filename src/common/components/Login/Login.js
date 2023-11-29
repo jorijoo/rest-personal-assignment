7e775bc0f1fd002571f3c247e4a264bc2e47b227
NicoK81 <n2kyni00@students.oamk.fi>
2023-11-29 12:00:10 +0200

    login-perhaps

--- src/common/components/Login/Login.js
+++ src/common/components/Login/Login.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { effect, signal } from '@preact/signals-react'
+
+
+
+
+    
+ 
 
 const Login = () => {
+
+const [username, setUsername] = useState('')
+const [pw, setPw] = useState('')
+const token = signal('')
+
+
+axios.postForm('http://localhost:3000/Login', {username, pw})
+    .then(resp => token.value = resp.data.jwtToken)
+    .catch(error => console.log(error.message))
+
   return (
     <div className='login_template d-flex justify-content-center align-items-center vh-100'>
         <div className='form_container p-5 rounded'>
@@ -10,11 +28,11 @@ const Login = () => {
             <h3 className='text-center'>Sign In</h3>
             <div className='mb-2'>
                 <label htmlFor="email">Email</label>
-                <input type="email" placeholder='Enter Email' className='form-control'/> 
+                <input value={username} onChange={e => setUsername(e.target.value)}  type="email" placeholder='Enter Email' className='form-control'/> 
             </div>
             <div className='mb-2'>
                 <label htmlFor="password">Password</label>
-                <input type="password" placeholder='Enter Password' className='form-control'/> 
+                <input value={pw} onChange={e => setPw(e.target.value)}type="password" placeholder='Enter Password' className='form-control'/> 
             </div>
             <div className='mb-2'>
                 <input type="checkbox" className='custom-control custom-checkbox'id="check"/>
@@ -23,7 +41,7 @@ const Login = () => {
                 </label>
             </div>
             <div className='d-grid'>
-                <button className='btn btn-primary'>Sign in</button>
+                <button onClick={Login} className='btn btn-primary'>Sign in</button>
             </div>
             <p className='text-end mt-2'>
                 Forgot <a href="">Password?</a> <Link to="/signup" className='ms-2'>Sign up</Link>
