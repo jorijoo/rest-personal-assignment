b5f0042c9af60e36128a4376cec5464057fd534c
laovirtanen <=>
2023-12-05 20:46:54 +0200

    changes on layout (table)

--- src/common/components/UserDashboard/userDashboard.js
+++ src/common/components/UserDashboard/userDashboard.js
@@ -1,49 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import { loginStatusSignal } from "../../signals/LoginStatusSignal";
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+
 
 const UserDashboard = () => {
   const navigate = useNavigate();
-
-  // Check login status using loginStatusSignal
+  const [orders, setOrders] = useState([]);
   const isLoggedIn = loginStatusSignal.value === "Login Successful";
 
-  // Log out
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        try {
+          const response = await axios.get("http://localhost:3001/myorders", {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+          setOrders(response.data);
+        } catch (error) {
+          console.error("Error fetching orders:", error);
+        }
+      }
+    };
+
+    if (isLoggedIn) {
+      fetchOrders();
+    }
+  }, [isLoggedIn]);
+
   const handleLogout = () => {
     try {
-      // Remove token from localStorage (you can keep this part)
       localStorage.removeItem("token");
-
-      // Update loginStatusSignal
       loginStatusSignal.value = "Login Failed";
-
-      // Navigate back to the home page using the navigate hook
       navigate("/");
-      
-      // Log or display a message to the user
       console.log('You have been logged out.');
     } catch (error) {
       console.error('Error during logout:', error);
     }
   };
 
-  console.log("UserDashboard - Login status:", isLoggedIn);
+  // // Laske tilauksen kokonaishinta (kaikkien tuotteiden hinnat yhteensä valitettavasti)
+  // const calculateTotalPrice = (order) => {
+  //   return order.reduce((total, product) => total + (product.price * product.quantity), 0);
+  // };
 
   return (
-    <div>
-      <h1>User Dashboard</h1>
+    <Container>
+      <h1 className="my-4">User Dashboard</h1>
       {isLoggedIn ? (
-        <div>
-          <p>You are logged in.</p>
-          <button onClick={handleLogout}>Log out</button>
-        </div>
+        <Container>
+          <h2 className="my-3">My Orders</h2>
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>Order ID</th>
+                <th>Order Date</th>
+                <th>Product Name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map(order => (
+                <tr key={order.orderId}>
+                  <td>{order.orderId}</td>
+                  <td>{order.orderDate}</td>
+                  <td>{order.productName}</td>
+                  <td>{order.price}</td>
+                  <td>{order.quantity}</td>
+                </tr>
+                
+              ))}
+            </tbody>
+          </Table>
+          <button onClick={handleLogout} className="btn btn-primary my-3">Log out</button>
+        </Container>
       ) : (
         <div>
           <p>You are not logged in.</p>
           <Link to="/login">Log in</Link>
         </div>
       )}
-    </div>
+    </Container>
   );
 };
 
