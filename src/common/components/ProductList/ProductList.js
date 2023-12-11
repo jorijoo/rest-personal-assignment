21336eff65b230bfc879b2281734c8a81febe8db
NicoK81 <n2kyni00@students.oamk.fi>
2023-12-11 13:05:57 +0200

    #150 product on front page

--- src/common/components/ProductList/ProductList.js
+++ src/common/components/ProductList/ProductList.js
@@ -9,29 +9,34 @@ export const ProductList = () => {
       price: "779.99€",
       image: "https://big.kapsi.fi/products/camera_retro.jpg",
       description: "Canon Retro 1970",
+      id: 1,
     },
     {
       price: "2989.99€",
       image: "https://big.kapsi.fi/products/camera_scoped.jpg",
       description: "Kodak Scope",
+      id: 6,
     },
 
     {
       price: "9.99€",
       image: "https://big.kapsi.fi/products/moustache_rainbow.jpg",
       description: "Viikset 3",
+      id: 11,
     },
 
     {
       price: "12.99€",
       image: "https://big.kapsi.fi/products/scale_model_tree_house.jpg",
       description: "Puutalo",
+      id: 16,
     },
 
     {
       price: "128.99€",
       image: "https://big.kapsi.fi/products/tattoo_pen_hairy_evil.jpg",
       description: "Parker 3",
+      id: 21,
     },
   ];
 
@@ -44,6 +49,7 @@ export const ProductList = () => {
               price={product.price}
               image={product.image}
               description={product.description}
+              id={product.id}
             />
           </div>
         ))}
