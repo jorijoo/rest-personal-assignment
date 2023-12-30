b8e2c8e0893f88c96a7c62053dc6e088322dcf06
Jori Hiltunen <hiltusenjori@gmail.com>
2023-12-30 17:00:58 +0200

    Show product reputation as satisfaction percentage 	modified:   src/common/components/ProductDetail/ProductDetail.js

--- src/common/components/ProductDetail/ProductDetail.js
+++ src/common/components/ProductDetail/ProductDetail.js
@@ -6,90 +6,109 @@ import { cartContentSignal } from "../../signals/CartSignals";
 import { ENV } from '../../constants/public_env'
 
 const ProductDetail = () => {
-  //Haetaan tuotteen id URL:sta käyttämällä params
-  let { id } = useParams();
+    //Haetaan tuotteen id URL:sta käyttämällä params
+    let { id } = useParams();
 
-  //Tilamuuttuja tuotteelle
-  const [product, setProduct] = useState(null);
+    //Tilamuuttuja tuotteelle
+    const [product, setProduct] = useState(null);
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const response = await axios.get(`${ENV.BACKEND}/products/${id}`);
-        setProduct(response.data);
+    useEffect(() => {
+        const fetchProduct = async () => {
+            try {
+                const response = await axios.get(`${ENV.BACKEND}/products/${id}`);
+                setProduct(response.data);
 
-      } catch (error) {
-        console.error("Virhe tuotteen haussa:", error);
-      }
+            } catch (error) {
+                console.error("Virhe tuotteen haussa:", error);
+            }
+        };
+
+        fetchProduct();
+    }, [id]);
+
+    if (!product) {
+        return <div>Ladataan...</div>;
+    }
+
+    /*
+    ------------------------------
+    Function to add selected item in cart
+    ------------------------------
+    */
+    const addToCart = async () => {
+        const currentProduct = cartContentSignal.value.find(p => p.id === product.id);
+
+        if (currentProduct) {
+            if (currentProduct.quantity >= product.unitsStored) {
+                alert(`Tuotetta ostoskorissasi on jo (${currentProduct.quantity}) kpl,varastosaldo ylittyy joten tuotetta ei lisätty ostoskoriin!`);
+                return;
+            }
+            currentProduct.quantity++;
+            cartContentSignal.value = [...cartContentSignal.value];
+        }
+        else {
+            cartContentSignal.value = [...cartContentSignal.value, { ...product, quantity: 1 }];
+        }
+
+        alert("Tuote '" + product.productName + "' lisätty ostoskoriin");
     };
 
-    fetchProduct();
-  }, [id]);
+    /**
+     * ----------------------------
+     * Report product reputation
+     * ----------------------------
+     */
+    const CheckRep = () => {
+        const repCount = +product.positiveReputation + +product.negativeReputation
 
-  if (!product) {
-    return <div>Ladataan...</div>;
-  }
+        const reputation = (repCount > 4)
+            ? Math.round(product.positiveReputation / repCount * 100)
+            : 'Ei tarpeeksi arvosteluita'
 
-  /*
-  ------------------------------
-  Function to add selected item in cart
-  ------------------------------
-  */
-  const addToCart = async () => {
-    const currentProduct = cartContentSignal.value.find(p => p.id === product.id);
-
-    if (currentProduct) {
-      if (currentProduct.quantity >= product.unitsStored) {
-        alert(`Tuotetta ostoskorissasi on jo (${currentProduct.quantity}) kpl,varastosaldo ylittyy joten tuotetta ei lisätty ostoskoriin!`);
-        return;
-      }
-      currentProduct.quantity++;
-      cartContentSignal.value = [...cartContentSignal.value];
-    }
-    else {
-      cartContentSignal.value = [...cartContentSignal.value, { ...product, quantity: 1 }];
+        return (
+            <p className="lead my-2">
+                Asiakkaiden tyytyväisyys tuotteeseen: {reputation} %
+            </p>
+        )
     }
 
-    alert("Tuote '" + product.productName + "' lisätty ostoskoriin");
-  };
+    /*
+    -----------------
+     To change stock number color if units stock is low : good
+    -----------------
+    */
+    const styleForStockNumber = {
+        color: product.unitsStored < 5 ? 'red' : 'green'
+    };
 
-  /*
-  -----------------
-   To change stock number color if units stock is low : good
-  -----------------
-  */
-  const styleForStockNumber = {
-    color: product.unitsStored < 5 ? 'red' : 'green'
-  };
-
-  return (
-    <div className="container mt-5 mb-5">
-      <div className="row">
-        <div className="col-md-6">
-          <img
-            src={product.imageUrl}
-            alt={product.productName}
-            className="img-fluid"
-          />
+    return (
+        <div className="container mt-5 mb-5">
+            <div className="row">
+                <div className="col-md-6">
+                    <img
+                        src={product.imageUrl}
+                        alt={product.productName}
+                        className="img-fluid"
+                    />
+                </div>
+                <div className="col-md-6">
+                    <div className="product-detail">
+                        <h1 className="display-4">{product.productName}</h1>
+                        <p className="lead">{product.productDescription}</p>
+                        <p className="lead">Varastossa: <b style={styleForStockNumber}>{product.unitsStored} </b> kpl</p>
+                        <p className="price h3">€{product.price}</p>
+                        <button className="btn btn-primary btn-lg"
+                            onClick={() => addToCart()}
+                            disabled={product.unitsStored === 0}>
+                            Lisää ostoskoriin
+                        </button>
+                        {product.unitsStored === 0 && <p className="text-danger">Tuote on loppu varastosta</p>}
+                        <CheckRep />
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="col-md-6">
-          <div className="product-detail">
-            <h1 className="display-4">{product.productName}</h1>
-            <p className="lead">{product.productDescription}</p>
-            <p className="lead">Varastossa: <b style={styleForStockNumber}>{product.unitsStored} </b> kpl</p>
-            <p className="price h3">€{product.price}</p>
-            <button className="btn btn-primary btn-lg"
-            onClick={() => addToCart()}
-            disabled={product.unitsStored === 0}
-            >
-            Lisää ostoskoriin
-            </button>
-            {product.unitsStored === 0 && <p className="text-danger">Tuote on loppu varastosta</p>}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProductDetail;
